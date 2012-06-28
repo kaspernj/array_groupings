2892c697c3cf738af42e6ee3f09d2971bc294290
Kasper Johansen <k@spernj.org>
2012-06-28 10:49:10 +0200

    Made it work.

--- lib/array_groupings.rb
+++ lib/array_groupings.rb
@@ -1,55 +1,57 @@
 class Array_groupings
   def initialize(args)
     @args = args
-    @arr = @args[:arr]
+    @arr = @args[:arr].to_enum
     @debug = @args[:debug]
   end
   
   def parse
     return Enumerator.new do |yielder|
-      prev_ele = @arr.shift
+      prev_ele = @arr.next
       chunk = [prev_ele]
       chunks = [chunk]
       print "First ele: #{prev_ele}\n" if @debug
       
-      @arr.each do |ele|
-        print "Ele: '#{ele}'\n" if @debug
-        deletes = []
-        adds = []
-        
-        chunks.each do |chunk_i|
-          res_last = yield(chunk_i.last, ele)
-          res_first = yield(chunk_i.first, ele)
+      begin
+        while ele = @arr.next
+          print "Ele: '#{ele}'\n" if @debug
+          deletes = []
+          adds = []
           
-          if res_first and res_last
-            chunk_i << ele
-          elsif !res_last and !res_first
-            yielder << chunk_i
-            deletes << chunk_i
-            adds << [ele]
-          elsif res_last and !res_first
-            new_add = []
-            chunk_i.each do |chunk_i_ele|
-              if yield(chunk_i_ele, ele)
-                new_add << chunk_i_ele
-              end
-            end
-            new_add << ele
+          chunks.each do |chunk_i|
+            res_last = yield(chunk_i.last, ele)
+            res_first = yield(chunk_i.first, ele)
             
-            yielder << chunk_i
-            adds << new_add
-            deletes << chunk_i
-          else
-            raise "Dont know what to do here..."
+            if res_first and res_last
+              chunk_i << ele
+            elsif !res_last and !res_first
+              yielder << chunk_i
+              deletes << chunk_i
+              adds << [ele]
+            elsif res_last and !res_first
+              new_add = []
+              chunk_i.each do |chunk_i_ele|
+                if yield(chunk_i_ele, ele)
+                  new_add << chunk_i_ele
+                end
+              end
+              new_add << ele
+              
+              yielder << chunk_i
+              adds << new_add
+              deletes << chunk_i
+            else
+              raise "Dont know what to do here. First: #{chunk_i.first}, last: #{chunk_i.last}, ele: #{ele}, res_last: #{res_last}, res_first: #{res_first}"
+            end
           end
+          
+          chunks -= deletes
+          chunks += adds
         end
-        
-        chunks -= deletes
-        chunks += adds
-      end
-      
-      chunks.each do |chunk|
-        yielder << chunk
+      rescue StopIteration
+        chunks.each do |chunk|
+          yielder << chunk
+        end
       end
     end
   end
